fe82f063f5b1cb50a8b25c21f4f16155323ea679
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2023-03-20 23:17:54 +0900

    apply classical mapping instead of declarative in modeling

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from database import models, session
 
-models.Base.metadata.create_all(bind=session.engine)
+# models.Base.metadata.create_all(bind=session.engines)
 
 app = FastAPI()
 
