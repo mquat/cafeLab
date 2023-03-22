febb6399c110e8e5001f437713b285962e25b95f
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2023-03-23 00:29:16 +0900

    cache settings

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from functools import lru_cache
 from fastapi import FastAPI
+
+from utils import config
 
 app = FastAPI()
 
+@lru_cache
+def get_settings():
+    return config.settings
+
+get_settings()
