2451db4d8ff74efe3d22fc862392654a13b95083
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2023-03-28 23:49:36 +0900

    remove lru cache & calling get_settings in main file

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from functools import lru_cache
 from fastapi import FastAPI
 
 from utils import config
 
 app = FastAPI()
 
-@lru_cache
 def get_settings():
     return config.settings
 
-get_settings()
+
