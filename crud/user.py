6ec0811ef5d8f02999ea2cfb41efc4c787447201
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2023-05-11 01:09:13 +0900

    break get-logout-user function

--- crud/user.py
+++ crud/user.py
@@ -1,8 +1,22 @@
+from datetime import datetime
+
+from jose import jwt, JWTError
 from sqlalchemy import or_, update
 from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
 
 from schemas.user import createUser
 from database.models import User
+from utils.config import settings, load_redis
+
+ALGORITHM = 'HS256'
+redis = load_redis()
+
+credentials_exception = HTTPException(
+    status_code = status.HTTP_401_UNAUTHORIZED,
+    detail = 'INVALID TOKEN',
+    headers = {'WWW-Authenticate': 'Bearer'}
+)
 
 def create_user(
     signup_info: createUser,
@@ -85,4 +99,38 @@ def get_user_info_by_user_id(
     if not user:
         return None
     return user
+
+def get_user_payload_by_token(
+    authorization: str,
+    db = Session
+)-> dict:
+    try:
+        payload = jwt.decode(authorization, settings.secret_key, algorithms=ALGORITHM)
+        user_id = payload.get('id')
+
+        if user_id is None:
+            raise credentials_exception
+
+        user = get_user_info_by_user_id(user_id, db)
+
+        if not user:
+            raise credentials_exception
+
+        return payload
+
+    except JWTError:
+        raise credentials_exception
+
+def delete_authorization_token(
+    user_authorization: str,
+    user_payload: dict,
+    expired_date: datetime
+)-> None:
+
+    expired_date = user_payload.get('exp')
+    current_date = int(datetime.utcnow().timestamp())
+
+    redis.setex(name=user_authorization, time=(expired_date-current_date), value='signed_out')
+
+    return
 
--- utils/auth.py
+++ utils/auth.py
@@ -1,18 +1,24 @@
 from datetime import datetime, timedelta
 
 from passlib.context import CryptContext
-from jose import jwt, JWTError
+from jose import jwt
 from typing import Union
 from fastapi import Depends, HTTPException, Header, status
 
 from .config import settings, load_redis
 from database.session import get_db
-from crud.user import get_user_info_by_user_id
+from crud.user import get_user_payload_by_token, delete_authorization_token
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 db = Depends(get_db)
 ALGORITHM = 'HS256'
 redis = load_redis()
+
+credentials_exception = HTTPException(
+    status_code = status.HTTP_401_UNAUTHORIZED,
+    detail = 'INVALID TOKEN',
+    headers = {'WWW-Authenticate': 'Bearer'}
+)
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
@@ -28,38 +34,14 @@ def create_access_token(payload:dict) -> str:
     return encoded_jwt
 
 def get_logout_user(authorization : Union[str, None] = Header(default=None), db = db):
-    credentials_exception = HTTPException(
-        status_code = status.HTTP_401_UNAUTHORIZED,
-        detail = 'INVALID TOKEN',
-        headers = {'WWW-Authenticate': 'Bearer'}
-    )
-
     if not authorization:
         raise credentials_exception
 
     if redis.get(authorization):
         raise credentials_exception
 
-    try:
-        payload = jwt.decode(authorization, settings.secret_key, algorithms=ALGORITHM)
+    user_payload = get_user_payload_by_token(authorization, db)
+    delete_authorization_token(authorization, user_payload, db)
 
-        user_id = payload.get('id')
-
-        if user_id is None:
-            raise credentials_exception
-
-        user = get_user_info_by_user_id(user_id, db)
-
-        if not user:
-            raise credentials_exception
-
-        expired_date = payload.get('exp')
-        current_date = int(datetime.utcnow().timestamp())
-
-        redis.setex(name=authorization, time=(expired_date-current_date), value='signed_out')
-
-        return user
-
-    except JWTError:
-        raise credentials_exception
+    return
 
