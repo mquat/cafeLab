52c2514c1dbfa3f7dba46f4f04b70d93dc90786f
Meeryu <meeryu@Meeryuui-MacBook-Pro.local>
2023-06-17 00:37:17 +0900

    add cafe info crawling

---
+++ scheduler/cafe_info.py
@@ -0,0 +1,39 @@
+import requests
+
+from utils.config import settings
+from crud.cafe import get_total_cafe_name_list, update_new_cafe
+from database.session import get_db
+
+GOOGLE_KEY = settings.google_api_key
+KAKAO_KEY  = settings.kakao_api_key
+
+def update_kakao_local_cafe():
+    db = next(get_db())
+
+    db_cafe_list = get_total_cafe_name_list(db)
+
+    url = f"https://dapi.kakao.com/v2/local/search/category.json?category_group_code=CE7"
+    header = {'Authorization': 'KakaoAK ' + KAKAO_KEY}
+
+    result = requests.get(url, headers=header).json()
+    kakao_cafe_list = result['documents']
+
+    new_cafe_list = []
+    if db_cafe_list:
+        for new_cafe in kakao_cafe_list:
+            for cafe in db_cafe_list:
+                if new_cafe['place_name'] == cafe[0]:
+                    break
+            else:
+                new_cafe_list.append(new_cafe)
+
+        for new_cafe in new_cafe_list:
+            update_new_cafe(new_cafe, db)
+
+        db.close()
+    else:
+        for new_cafe in kakao_cafe_list:
+            update_new_cafe(new_cafe, db)
+
+        db.close()
+
